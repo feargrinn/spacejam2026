6e5acbe73cf06e8ea07f420f978126e4b9a0206e
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-06-07 17:59:15 +0200

    Fixed reloading on lose

--- scenes/lose_screen.gd
+++ scenes/lose_screen.gd
@@ -1,7 +1,8 @@
 class_name LoseScreen
 extends PanelContainer
 
+@onready var sub_viewport: SubViewportWithLevel = $"../SubViewportContainer/SubViewport"
 
 func _on_button_pressed() -> void:
 	Player.instance.get_data().radiation = 0
-	get_tree().reload_current_scene()
+	sub_viewport.load_level()
